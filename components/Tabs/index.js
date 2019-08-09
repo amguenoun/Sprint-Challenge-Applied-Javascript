8f8c3a70ad230e0fdc8b37c80856b4ea77178b07
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-09 11:25:57 -0500

    completed axios get request and completed tabs js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,6 +7,16 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+        response.data.topics.forEach((item) => {
+            createTab(item);
+        });
+    })
+    .catch(error => console.log(`Error: ${error}`))
+
+
 const topics = document.querySelector('.topics');
 
 function createTab(topic) {
