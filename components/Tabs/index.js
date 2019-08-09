8bb256c5f83b9429611609fa406c1af7f16abf1e
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-09 11:21:39 -0500

    created createTab component function

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,12 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+const topics = document.querySelector('.topics');
+
+function createTab(topic) {
+    const tab = document.createElement('div');
+    tab.classList.add('tab');
+    tab.textContent = topic;
+
+    topics.appendChild(tab);
+}
